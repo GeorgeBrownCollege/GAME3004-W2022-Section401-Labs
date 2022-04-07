aaa88a368c9069d1d92260bd514aca2d3102efd3
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-04-07 10:23:56 -0400

    Added Project Files for Lab 8

--- GAME3004-W2022-Lab8/Assets/[Scripts]/MapGenerator.cs
+++ GAME3004-W2022-Lab8/Assets/[Scripts]/MapGenerator.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
@@ -9,17 +11,17 @@ public class MapGenerator : MonoBehaviour
     public Transform spawnPoint;
 
     [Header("World Properties")] 
-    [Range(8, 64)]
+    [Range(8, 128)]
     public int height = 8;
-    [Range(8, 64)]
+    [Range(8, 128)]
     public int width = 8;
-    [Range(8, 64)]
+    [Range(8, 128)]
     public int depth = 8;
 
     [Header("Scaling Values")]
-    [Range(8, 64)]
+    [Range(8, 128)]
     public float min = 16.0f;
-    [Range(8, 64)]
+    [Range(8, 128)]
     public float max = 24.0f;
 
     [Header("Tile Properties")] 
@@ -35,10 +37,49 @@ public class MapGenerator : MonoBehaviour
     private float startMin;
     private float startMax;
 
+    private Queue<GameObject> pool;
+
     // Start is called before the first frame update
     void Start()
     {
+        pool = new Queue<GameObject>();
+
+        for (int i = 0; i < 10000; i++)
+        {
+            CreateTile();
+        }
+
+
         Generate();
+    }
+
+    private void CreateTile()
+    {
+        var tile = Instantiate(threeDTile, Vector3.zero, Quaternion.identity);
+        tile.SetActive(false);
+        tile.transform.SetParent(tileParent);
+        pool.Enqueue(tile);
+    }
+
+    private GameObject GetTile(Vector3 position = new Vector3())
+    {
+        GameObject tile = null;
+        if (pool.Count < 1)
+        {
+            CreateTile();
+        }
+
+        tile = pool.Dequeue();
+        tile.SetActive(true);
+        tile.transform.position = position;
+        return tile;
+    }
+
+    private void ReleaseTile(GameObject tile)
+    {
+        tile.AddComponent<BoxCollider>();
+        tile.SetActive(false);
+        pool.Enqueue(tile);
     }
 
     // Update is called once per frame
@@ -62,6 +103,7 @@ public class MapGenerator : MonoBehaviour
         Regenerate();
         //DisableCollidersAndMeshRenderers();
         RemoveInternalTiles();
+        CombineMeshes();
         PositionPlayer();
     }
 
@@ -91,10 +133,13 @@ public class MapGenerator : MonoBehaviour
 
                     if (y < perlinValue)
                     {
-                        var tile = Instantiate(threeDTile, new Vector3(x, y, z), Quaternion.identity);
-                        tile.transform.SetParent(tileParent);
-                        grid.Add(tile);
+                        var tile = GetTile(new Vector3(x * threeDTile.transform.localScale.x,
+                            y * threeDTile.transform.localScale.y, z * threeDTile.transform.localScale.z));
 
+                        //var tile = Instantiate(threeDTile, new Vector3(x * threeDTile.transform.localScale.x, 
+                        //    y * threeDTile.transform.localScale.y, z * threeDTile.transform.localScale.z), Quaternion.identity);
+                        //tile.transform.SetParent(tileParent);
+                        grid.Add(tile);
                     }
                 }
             }
@@ -107,11 +152,21 @@ public class MapGenerator : MonoBehaviour
 
         for (int i = 0; i < size; i++)
         {
-            grid[i].GetComponent<BoxCollider>().enabled = false;
-            Destroy(grid[i]);
+            if (grid[i].GetComponent<BoxCollider>())
+            {
+                grid[i].GetComponent<BoxCollider>().enabled = false;
+            }
+
+            ReleaseTile(grid[i]);
         }
 
         grid.Clear();
+
+        if (tileParent.GetComponent<MeshCollider>())
+        {
+            Destroy(tileParent.GetComponent<MeshCollider>());
+        }
+       
     }
 
     private void DisableCollidersAndMeshRenderers()
@@ -172,19 +227,60 @@ public class MapGenerator : MonoBehaviour
             }
         }
 
+        // remove internal tiles
         var size = tilesToBeRemoved.Count;
         for (int i = 0; i < size; i++)
         {
             grid.Remove(tilesToBeRemoved[i]);
-            Destroy(tilesToBeRemoved[i].gameObject);
+            ReleaseTile(tilesToBeRemoved[i].gameObject);
+            //Destroy(tilesToBeRemoved[i].gameObject);
+        }
+
+        // remove box colliders from remaining tiles
+        foreach (var tile in grid)
+        {
+            Destroy(tile.GetComponent<BoxCollider>());
         }
     }
 
     private void PositionPlayer()
     {
         player.gameObject.GetComponent<CharacterController>().enabled = false;
-        player.position = new Vector3(width * 0.5f, height + 5.0f, depth * 0.5f);
+        player.position = new Vector3(width * 0.5f * threeDTile.transform.localScale.x, 
+            height * threeDTile.transform.localScale.y + 5.0f, depth * 0.5f * threeDTile.transform.localScale.z);
         spawnPoint.position = player.position;
         player.gameObject.GetComponent<CharacterController>().enabled = true;
+    }
+
+    private void CombineMeshes()
+    {
+        var meshFilter = tileParent.GetComponent<MeshFilter>();
+        meshFilter.mesh = new Mesh { indexFormat = UnityEngine.Rendering.IndexFormat.UInt32 };
+
+        List<MeshFilter> meshFilters = new List<MeshFilter>();
+
+
+        foreach (var tile in grid)
+        {
+            meshFilters.Add(tile.GetComponent<MeshFilter>());
+        }
+
+        CombineInstance[] combine = new CombineInstance[meshFilters.Count];
+
+
+        int i = 0;
+        while (i < meshFilters.Count)
+        {
+            //combine[i].mesh = new Mesh {indexFormat = UnityEngine.Rendering.IndexFormat.UInt32};
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            i++;
+        }
+
+        
+        meshFilter.mesh.CombineMeshes(combine);
+
+        tileParent.AddComponent<MeshCollider>();
     }
 }
